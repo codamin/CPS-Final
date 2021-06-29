f3ae111d448ce8c0f92875f495cc19d5e4b099f0
Amin Asadi <aminasadi329@gmail.com>
2021-06-29 12:14:39 +0430

    clean main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,8 @@ void setup() {
   motor.write(90);
   Serial.begin(9600);
 
-  set_admin("hoenza", "8586858600000000");
-  User user1 = addUser("hoenza", "8586858600000000");
-  User user2 = addUser("03:00:00:00:00:00", "1234567800000000");
+  set_admin("hoenza", "1010101000000000");
+  User user1 = addUser("hoenza", "1010101000000000");
 }
 
 void loop() {
